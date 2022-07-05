7671d290430e7b42d21728f07ce1afb702b78ee1
herves <toundaherve@gmail.com>
2022-07-05 23:02:17 +0100

    Set Env vars

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const SHARP = require("sharp");
 const s3 = new SDK.S3();
 
 // pull in environment variables that we specified in lambda settings
-const BUCKET = "bonvih-photos-of-items";
-const BUCKET_STATIC_WEBSITE_URL =
-  "http://bonvih-photos-of-items.s3-website.eu-west-2.amazonaws.com";
+const BUCKET_NAME = process.env.BUCKET_NAME;
 
 // export handler function that is needed for lambda execution
 exports.handler = async function (event, context, callback) {
@@ -24,11 +22,13 @@ exports.handler = async function (event, context, callback) {
 
   // fetch the original image from S3
   try {
-    var origimage = await s3.getObject({ Bucket: BUCKET, Key: path }).promise();
+    var origimage = await s3
+      .getObject({ Bucket: BUCKET_NAME, Key: path })
+      .promise();
   } catch (error) {
     console.log("Could not get object from s3");
     console.log(error);
-    // callback(error, null);
+
     return {
       statusCode: "400",
       body: JSON.stringify(error),
@@ -44,7 +44,7 @@ exports.handler = async function (event, context, callback) {
   } catch (error) {
     console.log("Could not resize photo");
     console.log(error);
-    // callback(error, null);
+
     return {
       statusCode: "400",
       body: JSON.stringify(error),
@@ -55,63 +55,27 @@ exports.handler = async function (event, context, callback) {
     await s3
       .putObject({
         Body: buffer,
-        Bucket: BUCKET,
+        Bucket: BUCKET_NAME,
         Key: key,
         ContentType: "image/jpeg",
         ContentDisposition: "inline", // ensure that the browser will display S3 images inline
       })
       .promise();
 
-    // callback(null, {
-    //   statusCode: "301",
-    //   headers: { location: `${BUCKET_STATIC_WEBSITE_URL}/${key}` },
-    //   body: "",
-    // });
     return {
       statusCode: "301",
-      headers: { location: `${BUCKET_STATIC_WEBSITE_URL}/${key}` },
+      headers: {
+        location: `http://${BUCKET_NAME}.s3-website.eu-west-2.amazonaws.com/${key}`,
+      },
       body: "",
     };
   } catch (error) {
     console.log("Could not upload photo to s3");
     console.log(error);
-    // callback(error, null);
+
     return {
       statusCode: "400",
       body: JSON.stringify(error),
     };
   }
-
-  // S3.getObject({ Bucket: BUCKET, Key: path }, (err, data) => {
-  //   if (err) {
-  //     callback(err, null);
-  //   }
-  //   // use Sharp (https://www.npmjs.com/package/sharp)
-  //   // a node.js image conversion library, to resize the image.
-  //   // .rotate.resize(width, height === 0 ? undefined : height)
-  //   SHARP(data.Body)
-  //     .resize(width, height)
-  //     .jpeg({ mozjpeg: true })
-  //     .toBuffer()
-  //     .then((buffer) =>
-  //       // create a new entry in S3 with our resized image
-  //       // the key is unique per size - i.e. 300x300/image.jpg
-  //       S3.putObject(
-  //         {
-  //           Body: buffer,
-  //           Bucket: BUCKET,
-  //           Key: key,
-  //           ContentType: "image/jpeg",
-  //           ContentDisposition: "inline", // ensure that the browser will display S3 images inline
-  //         },
-  //         () =>
-  //           // generate lambda response with the location of the newly uploaded file
-  //           callback(null, {
-  //             statusCode: "301",
-  //             headers: { location: `${BUCKET_STATIC_WEBSITE_URL}/${key}` },
-  //             body: "",
-  //           })
-  //       )
-  //     );
-  // });
 };
